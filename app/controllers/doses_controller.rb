cc58fc3861c5a8e91bf6b360e50e66a5dddb359e
Joscha Koepke <joscha.koepke@gmail.com>
2018-03-01 19:37:31 +0100

    Backend

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+
+  def create
+    # Add user input to empty dose object
+    @dose = Dose.new(doses_params)
+    # Get missing cocktail id from url params
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # Specify that the cocktail of the dose is the one we found
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "/cocktails/show"
+    end
+    # flash[:alert] = @dose.errors.full_messages unless @dose.save
+    # redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    dose = Dose.find(params[:id])
+    dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+    def doses_params
+      params.require(:dose).permit(:description, :ingredient_id)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :cocktails, only: [ :new, :create, :show, :index] do
+    resources :doses, only: [ :create]
+  end
+  resources :doses, only: [ :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
